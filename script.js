274d30f68e9c8a33d40ffacbaa847207cd98da9f
pkc98820-maker <pkc98820@gmail.com>
2026-02-14 11:22:14 +0530

    Initial commit of assignment 1

--- script.js
+++
@@ -1,7 +0,0 @@
-const form =document.getElementById("form");
-
-form.addEventListener("submit",(event) =>{
-    console.log(event.target.id);
-// it prevents autorefresh
-  event.preventDefault();    
-})
